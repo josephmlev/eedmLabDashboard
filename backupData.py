d76e49a8f6f6680b8371d84c6038bc0e2ae8a034
eedm dashboard pi <josephmlevine123@gmail.com>
2026-01-27 15:45:57 -0800

    working on backups

--- backupData.py
+++ backupData.py
@@ -3,9 +3,10 @@ import subprocess
 from datetime import datetime
 
 date_str = datetime.now().strftime('%Y-%m-%d')
-shutil.copy('data.json', f'backups/data_{date_str}.json')
+filename = f'backups/data_{date_str}5.json'
+shutil.copy('data.json', filename)
 
-subprocess.run(['git', 'add', f'data_{date_str}.json'])
+subprocess.run(['git', 'add', filename])
 subprocess.run(['git', 'commit', '-m', f'Archive {date_str}'])
 subprocess.run(['git', 'pull', '--rebase'])
 subprocess.run(['git', 'push'])
